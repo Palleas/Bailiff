9fb39fbc4ba621f62da8675bd11bab00c8b839a8
Romain Pouclet <romain.pouclet@gmail.com>
2016-04-16 17:34:09 -0400

    Bootstrap all the things

---
+++ Arwing/Arwing.h
@@ -0,0 +1,19 @@
+//
+//  Arwing.h
+//  Arwing
+//
+//  Created by Romain Pouclet on 2016-04-16.
+//  Copyright © 2016 Perfectly-Cooked. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Arwing.
+FOUNDATION_EXPORT double ArwingVersionNumber;
+
+//! Project version string for Arwing.
+FOUNDATION_EXPORT const unsigned char ArwingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Arwing/PublicHeader.h>
+
+
---
+++ Arwing/Client.swift
@@ -0,0 +1,14 @@
+//
+//  Cliebt.swift
+//  Bailiff
+//
+//  Created by Romain Pouclet on 2016-04-16.
+//  Copyright © 2016 Perfectly-Cooked. All rights reserved.
+//
+
+import Cocoa
+
+class Client: NSObject {
+
+    
+}
